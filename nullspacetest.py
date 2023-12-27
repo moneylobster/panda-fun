0899715eeea3d19cee51d6b893e2c07cf05cda9e
moneylobster <87999452+moneylobster@users.noreply.github.com>
2023-12-27 13:09:57 +0300

    play all in seq.

--- nullspacetest.py
+++ nullspacetest.py
@@ -10,17 +10,16 @@ from panda_py import controllers
 import spatialmath.base as smb
 
 
-if len(sys.argv)==1:
-  raise RuntimeError("Please specify cartesian, joint or jointpos")
-ctrl_mode=sys.argv[1]
+if len(sys.argv)!=3:
+  raise RuntimeError("Please specify trans_stiff and rot_stiff")
+
+tra_stiff=sys.argv[1] #default is 200
+rot_stiff=sys.argv[2] #default is 10
 
 panda=panda_py.Panda("10.0.0.2")
-
 panda.move_to_start()
 
 LEN = 8
-tra_stiff=200 #default is 200
-rot_stiff=20 #default is 10
 
 input(f'Next, teach a trajectory for {LEN} seconds. Press enter to begin.')
 panda.teaching_mode(True)
@@ -44,7 +43,41 @@ print(f"pos/rot for first: {poss[0]} {rots[0]}")
 q = panda.get_log()['q']
 dq = panda.get_log()['dq']
 
-input('Press enter to replay trajectory')
+##########################################################
+input('Press enter to replay: JOINT POS')
+panda.move_to_joint_position(q[0])
+i = 0
+
+ctrl = controllers.JointPosition()
+
+panda.start_controller(ctrl)
+with panda.create_context(frequency=1000, max_runtime=LEN) as ctx:
+    while ctx.ok():
+        ctrl.set_control(q[i], dq[i]) #joint pos.
+        i += 1
+##########################################################
+input('Press enter to replay: CARTESIAN IMP., DEFAULT NULLSPACE')
+panda.move_to_joint_position(q[0])
+i = 0
+
+ctrl = controllers.CartesianImpedance(
+    impedance=np.array(
+        [[tra_stiff,0,0,0,0,0],
+         [0,tra_stiff,0,0,0,0],
+         [0,0,tra_stiff,0,0,0],
+         [0,0,0,rot_stiff,0,0],
+         [0,0,0,0,rot_stiff,0],
+         [0,0,0,0,0,rot_stiff]]),
+    filter_coeff=1.0)
+
+panda.start_controller(ctrl)
+with panda.create_context(frequency=1000, max_runtime=LEN) as ctx:
+    while ctx.ok():
+        ctrl.set_control(poss[i], rots[i]) #cartesian imp.
+        i += 1
+
+##########################################################
+input('Press enter to replay: CARTESIAN IMP., NULLSPACE=q')
 panda.move_to_joint_position(q[0])
 i = 0
 
@@ -59,15 +92,8 @@ ctrl = controllers.CartesianImpedance(
   filter_coeff=1.0)
 
 panda.start_controller(ctrl)
-if (sys.argv[1]=="0"):
-  with panda.create_context(frequency=1000, max_runtime=LEN) as ctx:
+      
+with panda.create_context(frequency=1000, max_runtime=LEN) as ctx:
     while ctx.ok():
-      ctrl.set_control(poss[i], rots[i]) #cartesian imp.
-      # ctrl.set_control(q[i], dq[i]) #joint pos.
-      i += 1
-else:
-  with panda.create_context(frequency=1000, max_runtime=LEN) as ctx:
-    while ctx.ok():
-      ctrl.set_control(poss[i], rots[i], q[i]) #cartesian imp.
-      # ctrl.set_control(q[i], dq[i]) #joint pos.
-      i += 1
+        ctrl.set_control(poss[i], rots[i], q[i]) #cartesian imp.
+        i += 1
