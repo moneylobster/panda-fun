80539490d41bf5e115cfa659d2de7cdb44ef8b16
moneylobster <87999452+moneylobster@users.noreply.github.com>
2023-12-27 13:37:00 +0300

    remove pyplot

--- nullspacetest.py
+++ nullspacetest.py
@@ -9,9 +9,6 @@ import panda_py
 from panda_py import controllers
 import spatialmath.base as smb
 
-import matplotlib.pyplot as plt
-
-
 if len(sys.argv)!=3:
   raise RuntimeError("Please specify trans_stiff and rot_stiff")
 
@@ -113,20 +110,3 @@ cartimplog=panda.get_log()
 cartimpq=cartimplog['q']
 np.save("cartimpdefq.npy",cartimpq)
 
-### plotting
-
-def plot_joints(fig,data,name):
-    "plot all joints onto different subplots"
-    data=np.array(data)
-    data=data.reshape(7,-1)
-    for i,val in enumerate(data.T):
-        plt.subplot(7,1,i)
-        plt.plot(val,name)
-
-fig=plt.figure()
-
-plot_joints(fig,jointposq, "jointpos")
-plot_joints(fig,cartimpdefq, "cartimpdef")
-plot_joints(fig,cartimpq, "cartimp")
-fig.legend(loc="best")
-fig.savefig("qvals.png")
