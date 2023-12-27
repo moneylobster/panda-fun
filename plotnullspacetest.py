d4e74d2fe69c2f7630dc4e562cbf498bbc9e65fc
moneylobster <87999452+moneylobster@users.noreply.github.com>
2023-12-27 13:51:01 +0300

    fix

--- plotnullspacetest.py
+++ plotnullspacetest.py
@@ -11,14 +11,16 @@ def plot_joints(fig,data,name):
     "plot all joints onto different subplots"
     data=np.array(data)
     data=data.reshape(7,-1)
-    for i,val in enumerate(data.T):
-        plt.subplot(7,1,i)
-        plt.plot(val,name)
+    print(data.shape)
+    for i,val in enumerate(data):
+        print(i,val)
+        plt.subplot(7,1,i+1)
+        plt.plot(val,label=name)
 
 fig=plt.figure()
 
 plot_joints(fig,jointposq, "jointpos")
 plot_joints(fig,cartimpdefq, "cartimpdef")
 plot_joints(fig,cartimpq, "cartimp")
-fig.legend(loc="best")
+plt.legend(loc="best")
 fig.savefig("qvals.png")
