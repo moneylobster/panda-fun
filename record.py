f2105d30b02c3e18bb799b8ce7bbe8ec705f7e16
moneylobster <87999452+moneylobster@users.noreply.github.com>
2023-12-27 12:18:18 +0300

    add changeable things

--- record.py
+++ record.py
@@ -3,18 +3,18 @@ operate arm and record as binarized np array
 '''
 import numpy as np
 import time
+import sys
 
 import panda_py
 from panda_py import controllers
 import spatialmath.base as smb
-# import swift
 
+
+if len(sys.argv)==1:
+  print("Please specify cartesian, joint or jointpos")
+ctrl_mode=sys.argv[1]
 
 panda=panda_py.Panda("10.0.0.2")
-
-# env = swift.Swift()
-# env.launch(realtime=True)
-# simpanda = rtb.models.Panda()
 
 panda.move_to_start()
 
@@ -40,6 +40,8 @@ panda.teaching_mode(False)
 
 log=panda.get_log()
 print(log.keys())
+
+# for cartesian ctrl
 endeffs=np.array(log["O_T_EE"])
 endeffs=endeffs.reshape((-1, 4, 4)).transpose((0, 2, 1))
 poss=[endeff[:3,3] for endeff in endeffs]
@@ -48,30 +50,33 @@ rots=[smb.r2q(endeff[:3,:3],order="xyzs") for endeff in endeffs]
 print(f"endeffs: {endeffs[0:2]} total size:{endeffs.shape}")
 print(f"pos/rot for first: {poss[0]} {rots[0]}")
 
+# for joint ctrl
 q = panda.get_log()['q']
 dq = panda.get_log()['dq']
 
 input('Press enter to replay trajectory')
 panda.move_to_joint_position(q[0])
 i = 0
-# ctrl = controllers.JointPosition()
-ctrl = controllers.CartesianImpedance(filter_coeff=1.0)
+
+if ctrl_mode=="jointpos":
+  ctrl = controllers.JointPosition()
+  arg1=q
+  arg2=dq
+elif ctrl_mode=="joint":
+  raise NotImplementedError("Joint Impedance isn't implemented yet")
+  ctrl = controllers.JointImpedance()
+  arg1=q
+elif ctrl_mode=="cartesian":
+  ctrl = controllers.CartesianImpedance(filter_coeff=1.0)
+  arg1=poss
+  arg2=rots
+else:
+  raise Exception(f"Unknown controller mode {ctrl_mode}")
+  
 panda.start_controller(ctrl)
 with panda.create_context(frequency=1000, max_runtime=LEN) as ctx:
   while ctx.ok():
-    ctrl.set_control(poss[i], rots[i])
-    # ctrl.set_control(q[i], dq[i])
+    # ctrl.set_control(poss[i], rots[i]) #cartesian imp.
+    # ctrl.set_control(q[i], dq[i]) #joint pos.
+    ctrl.set_control(arg1[i], arg2[i])
     i += 1
-    # simpanda.fkine(panda.q)
-
-# ctrl = controllers.CartesianImpedance(filter_coeff=1.0)
-# x0 = panda.get_position()
-# q0 = panda.get_orientation()
-# runtime = np.pi * 4.0
-# panda.start_controller(ctrl)
-
-# with panda.create_context(frequency=1e3, max_runtime=runtime) as ctx:
-#     while ctx.ok():
-#         x_d = x0.copy()
-#         x_d[1] += 0.1 * np.sin(ctrl.get_time())
-#         ctrl.set_control(x_d, q0)
