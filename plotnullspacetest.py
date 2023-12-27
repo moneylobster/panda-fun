3fc5aa2e2cb9c2ab7aeb5f4459810243c314206c
moneylobster <87999452+moneylobster@users.noreply.github.com>
2023-12-27 14:00:53 +0300

    plot works

--- plotnullspacetest.py
+++ plotnullspacetest.py
@@ -10,14 +10,14 @@ cartimpq=np.load("cartimpq.npy")
 def plot_joints(fig,data,name):
     "plot all joints onto different subplots"
     data=np.array(data)
-    data=data.reshape(7,-1)
+    data=data.reshape(-1,7)
     print(data.shape)
-    for i,val in enumerate(data):
+    for i,val in enumerate(data.T):
         print(i,val)
         plt.subplot(7,1,i+1)
         plt.plot(val,label=name)
 
-fig=plt.figure(figsize=(100,100))
+fig=plt.figure(figsize=(20,10))
 
 plot_joints(fig,jointposq, "jointpos")
 plot_joints(fig,cartimpdefq, "cartimpdef")
