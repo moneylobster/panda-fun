fab43ced870b2f1d8b7cf04701bd468c60c36df7
moneylobster <87999452+moneylobster@users.noreply.github.com>
2024-01-03 12:37:36 +0300

    add this thing

---
+++ sim_playback.py
@@ -0,0 +1,31 @@
+'''
+play back a recording made in the simulator
+'''
+import sys
+
+import numpy as np
+import roboticstoolbox as rtb
+from swift import Swift
+
+def playback(logname, stride):
+    log=np.load(logname, allow_pickle=True)
+    log=log.item()
+    # print(log.keys())
+    qs=np.array(log["q"])
+    
+    # create sim env
+    env=Swift()
+    env.launch(realtime=True)
+    panda=rtb.models.Panda()
+    env.add(panda)
+    dt=0.001*stride
+    for q in qs[::stride]:
+        panda.q=q
+        env.step(dt)
+    
+if __name__=="__main__":
+    if len(sys.argv)<2:
+        raise RuntimeError("Please supply the name of the .npy file to play.")
+    logname=sys.argv[1]
+    stride=int(sys.argv[2]) if len(sys.argv)>2 else 50
+    playback(logname, stride)
