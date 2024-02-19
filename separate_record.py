896c5dc3924b18c0667ab5b3cba3066fea2ab18c
moneylobster <87999452+moneylobster@users.noreply.github.com>
2024-02-19 18:43:14 +0300

    change how depth flag is handled

--- separate_record.py
+++ separate_record.py
@@ -80,24 +80,23 @@ while time.time()-t_start<=LEN:
         continue
 
     # Convert images to numpy arrays
-    if DEPTH:
-      depth_image = np.asanyarray(depth_frame.get_data())
+    depth_image = np.asanyarray(depth_frame.get_data())
     color_image = np.asanyarray(color_frame.get_data())
 
-    if DEPTH:
-      # Apply colormap on depth image (image must be converted to 8-bit per pixel first)
-      depth_colormap = cv2.applyColorMap(cv2.convertScaleAbs(depth_image, alpha=0.03), cv2.COLORMAP_JET)
+    # Apply colormap on depth image (image must be converted to 8-bit per pixel first)
+    depth_colormap = cv2.applyColorMap(cv2.convertScaleAbs(depth_image, alpha=0.03), cv2.COLORMAP_JET)
 
-      depth_colormap_dim = depth_colormap.shape
-      color_colormap_dim = color_image.shape
+    depth_colormap_dim = depth_colormap.shape
+    color_colormap_dim = color_image.shape
       
-      # If depth and color resolutions are different, resize color image to match depth image for display
-      if depth_colormap_dim != color_colormap_dim:
-        resized_color_image = cv2.resize(color_image, dsize=(depth_colormap_dim[1], depth_colormap_dim[0]), interpolation=cv2.INTER_AREA)
-        images = np.hstack((resized_color_image, depth_colormap))
-      else:
-        images = np.hstack((color_image, depth_colormap))
+    # If depth and color resolutions are different, resize color image to match depth image for display
+    if depth_colormap_dim != color_colormap_dim:
+      resized_color_image = cv2.resize(color_image, dsize=(depth_colormap_dim[1], depth_colormap_dim[0]), interpolation=cv2.INTER_AREA)
+      images = np.hstack((resized_color_image, depth_colormap))
     else:
+      images = np.hstack((color_image, depth_colormap))
+
+    if not DEPTH:
       images=color_image
     
     # save img
