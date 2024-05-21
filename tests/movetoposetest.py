78e4323ff7fb486416534e987c4ec48080bc3ccb
moneylobster <87999452+moneylobster@users.noreply.github.com>
2024-05-21 18:51:50 +0300

    testing an alternative

--- tests/movetoposetest.py
+++ tests/movetoposetest.py
@@ -9,6 +9,14 @@ print(f"Current pose is {current_pose}")
 
 newpose=SE3(current_pose)+SE3(0.1,0,0)
 
+print(newpose.data)
 newrot=newpose.data[0][:3,:3]
 
-panda.move_to_pose(newpose.t,st.Rotation.from_matrix(newrot).as_quat())
+# panda.move_to_pose(newpose.t,st.Rotation.from_matrix(newrot).as_quat())
+
+### ALTERNATE APPROACH:
+
+newq=panda_py.ik(newpose.t,st.Rotation.from_matrix(newrot).as_quat()), q_init=panda.q)
+print(f"old q:{panda.q} new q:{newq}")
+
+panda.move_to_joint_position(newq)
