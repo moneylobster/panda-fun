7b1716c4c1bba848abaab6bd25d1ec0070ee2597
moneylobster <87999452+moneylobster@users.noreply.github.com>
2024-07-29 13:14:58 +0300

    add homing and vacuum

--- skill_utils/teleop_2d.py
+++ skill_utils/teleop_2d.py
@@ -9,9 +9,10 @@ from skill_utils.teleop import KeyboardCommandHandler
 from skill_utils.format_pose import to_format, from_format
 
 class KeyboardPoseController(KeyboardCommandHandler):
-    def __init__(self, pose):
+    def __init__(self, pose, homeq=None):
         self.moveeps=0.01
         self.pose=pose
+        self.homeq=homeq
         
         super().__init__()
 
@@ -38,3 +39,37 @@ class KeyboardPoseController(KeyboardCommandHandler):
     def d(self):
         # left
         self.pose=SE3.Trans(0,self.moveeps,0) * self.pose
+
+    def r(self):
+        # home
+        if self.real:
+            #real
+            if self.homeq==None:
+                self.panda.move_to_start()
+            else:
+                self.panda.move_to_joint_position(self.homeq)
+            self.update_endeff()
+            self.panda.start_controller(self.ctrl)
+        else:
+            #sim
+            self.panda.q=self.panda.qr
+            self.update_endeff()
+            self.env.step(0.05)
+
+    def m(self):
+        # vacuum
+        # is vacuum on?
+        state=self.gripper.read_once()
+        if state.part_present:
+            try:
+                self.gripper.drop_off(self.gripeps)
+            except:
+                # if unsuccessful
+                self.gripper.stop()
+        else:
+            try:
+                self.gripper.vacuum(3,self.gripeps)
+            except:
+                # if unsuccessful
+                self.gripper.stop()
+
