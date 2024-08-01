ca6915ced054e8499de9d8bfcabb84a3c9efdba4
moneylobster <87999452+moneylobster@users.noreply.github.com>
2024-08-01 20:37:01 +0300

    trying automatic camera settings

--- diff_policy_configs/demo_panda.py
+++ diff_policy_configs/demo_panda.py
@@ -63,9 +63,12 @@ def main(output, robot_ip, vis_camera_idx, init_joints, frequency, command_laten
             cv2.setNumThreads(1)
 
             # realsense exposure
-            env.realsense.set_exposure(exposure=120, gain=0)
+            # env.realsense.set_exposure(exposure=120, gain=0)
+            env.realsense.set_exposure()
             # realsense white balance
-            env.realsense.set_white_balance(white_balance=5900)
+            # env.realsense.set_white_balance(white_balance=5900)
+            env.realsense.set_white_balance()
+            
             time.sleep(1.0)
 
             currentpose=np.reshape(env.get_robot_state()['ActualTCPPose'],(4,4)).T
