efe04f7b623b3c24d3f45d595ff2214fdfce5bbb
moneylobster <87999452+moneylobster@users.noreply.github.com>
2024-11-06 18:25:18 +0300

    implement the thing in torch

--- diff_policy_configs/diffusion_policy/model/vision/multi_image_obs_encoder_afterimage.py
+++ diff_policy_configs/diffusion_policy/model/vision/multi_image_obs_encoder_afterimage.py
@@ -1,3 +1,4 @@
+from typing import Dict, Tuple, Union
 import torch
 import torch.nn as nn
 from diffusion_policy.model.vision.multi_image_obs_encoder import MultiImageObsEncoder
@@ -15,21 +16,26 @@ import numpy as np
 #     "action": Tensor of shape (Ta, Da)
 # Its get_normalizer method returns a LinearNormalizer with keys "key0","key1","action".
 
-def afterimage_schedule(sch_type, To):
-    """Create a schedule. To is the obs. horizon.
-    Schedule types:
-    Linear
-    """
-    if sch_type=="linear":
-        return np.linspace(0,1,To)
-    else:
-        raise NotImplementedError(f"Unsupported schedule type {sch_type}")
-
-def create_afterimage(images, schedule):
-    """Create an afterimage from images according to weight schedule.
-    Schedule is a vector of weights."""
-    return np.average(images, axis=0, weights=schedule)
-
+class AfterimageGenerator():
+    def __init__(self, n_obs_steps, schedule_type):
+        self.schedule = self.create_schedule(n_obs_steps, schedule_type)
+        
+    def create_schedule(self, n_obs_steps, schedule_type):
+        """Create a schedule. n_obs_steps is the obs. horizon.
+        Schedule types:
+        - linear
+        """
+        if schedule_type=="linear":
+            return torch.linspace(0,1,n_obs_steps)
+        else:
+            raise NotImplementedError(f"Unsupported schedule type {schedule_type}")
+    
+    def forward(self, images):
+        """Create an afterimage from images according to weight schedule."""
+        # basically a weighted avg
+        return (self.schedule.view(self.schedule.shape[0],1,1,1) * images).sum(dim=0)/self.schedule.sum() # way slower than the np version idk why
+        # return np.average(images, axis=0, weights=self.schedule)
+    
 def test_create_afterimage():
     import skimage.io as io
     import numpy as np
@@ -37,7 +43,8 @@ def test_create_afterimage():
     for i in range(1,10):
         imgs.append(np.roll(imgs[0],i*10,1))
     print(np.array(imgs).shape)
-    res=create_afterimage(np.array(imgs), np.linspace(0,1,10))
+    ag=AfterimageGenerator(10, "linear")
+    res=ag.forward(torch.Tensor(imgs))
     print(res.shape)
     return res
     
@@ -47,6 +54,30 @@ class MultiImageObsEncoderAfterimage(MultiImageObsEncoder):
     generate the encoding, we do not pass each image from the model,
     instead we compute an afterimage and pass that through.
     """
+
+    def __init__(self,
+                 shape_meta: dict,
+                 rgb_model: Union[nn.Module, Dict[str,nn.Module]],
+                 n_obs_steps,
+                 resize_shape: Union[Tuple[int,int], Dict[str,tuple], None]=None,
+                 crop_shape: Union[Tuple[int,int], Dict[str,tuple], None]=None,
+                 random_crop: bool=True,
+                 # replace BatchNorm with GroupNorm
+                 use_group_norm: bool=False,
+                 # use single rgb model for all rgb inputs
+                 share_rgb_model: bool=False,
+                 # renormalize rgb input with imagenet normalization
+                 # assuming input in [0,1]
+                 imagenet_norm: bool=False
+                 ):
+        """
+        Assumes rgb input: B,C,H,W
+        Assumes low_dim input: B,D
+        """
+        super().__init__()
+        self.n_obs_steps = n_obs_steps
+        self.ke
+    
     def forward(self, obs_dict):
         print("DEBUGG")
         print(f"Obsdict: {obs_dict}")
@@ -80,7 +111,8 @@ class MultiImageObsEncoderAfterimage(MultiImageObsEncoder):
             # run each rgb obs to independent models
             for key in self.rgb_keys:
                 img = obs_dict[key]
-                print(f"imgshape {img.shape}")
+                print(f"imgshape {img.shape}") # yields torch.Size([1, 3, 240, 320])
+                # or 128, 3, 240, 320
                 if batch_size is None:
                     batch_size = img.shape[0]
                 else:
@@ -89,8 +121,8 @@ class MultiImageObsEncoderAfterimage(MultiImageObsEncoder):
                 img = self.key_transform_map[key](img)
                 feature = self.key_model_map[key](img)
                 features.append(feature)
-                print(f"lastimgshape {img.shape}")
-                print(f"featshape {feature.shape}")
+                print(f"lastimgshape {img.shape}") # yields torch.Size([1, 3, 216, 288])
+                print(f"featshape {feature.shape}") # yields torch.Size([1, 512]) or 128,512
         
         # process lowdim input
         for key in self.low_dim_keys:
