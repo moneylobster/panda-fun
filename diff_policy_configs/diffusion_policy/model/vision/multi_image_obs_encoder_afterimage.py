3ce5fdb824dbdacaacd041dbf794554734a3dc64
moneylobster <87999452+moneylobster@users.noreply.github.com>
2024-11-26 20:46:14 +0300

    improve comments idk

--- diff_policy_configs/diffusion_policy/model/vision/multi_image_obs_encoder_afterimage.py
+++ diff_policy_configs/diffusion_policy/model/vision/multi_image_obs_encoder_afterimage.py
@@ -80,7 +80,8 @@ class MultiImageObsEncoderAfterimage(MultiImageObsEncoder):
             # run each rgb obs to independent models
             for key in self.rgb_keys:
                 img = obs_dict[key]
-                print(f"imgshape {img.shape}") # yields torch.Size([To(or To*B), 3, 240, 320])
+                print(f"imgshape {img.shape}")
+                # should yield torch.Size([aft_horizon+To-1(*batch if training), 3, 240, 320])
                 if batch_size is None:
                     batch_size = img.shape[0]
                 else:
