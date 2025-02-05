fcef7c5138b378e29afd55b9c27aba8e98872873
moneylobster <87999452+moneylobster@users.noreply.github.com>
2025-02-05 16:25:07 +0300

    Trying out distributed training

---
+++ diff_policy_configs/distributed_train.py
@@ -0,0 +1,43 @@
+"""
+Usage:
+Training:
+python distributed_train.py --config-name=train_diffusion_lowdim_workspace +world_size=3
+"""
+
+import sys
+# use line-buffering for both stdout and stderr
+sys.stdout = open(sys.stdout.fileno(), mode='w', buffering=1)
+sys.stderr = open(sys.stderr.fileno(), mode='w', buffering=1)
+
+import torch.multiprocessing as mp
+
+import hydra
+from omegaconf import OmegaConf
+import pathlib
+from diffusion_policy.workspace.base_workspace import BaseWorkspace
+
+# allows arbitrary python code execution in configs using the ${eval:''} resolver
+OmegaConf.register_new_resolver("eval", eval, replace=True)
+
+def printargs(*args):
+    print(args)
+
+@hydra.main(
+    version_base=None,
+    config_path=str(pathlib.Path(__file__).parent.joinpath(
+        'diffusion_policy','config')),
+)
+def main(cfg: OmegaConf):
+    # resolve immediately so all the ${now:} resolvers
+    # will use the same time.
+    OmegaConf.resolve(cfg)
+
+    cls = hydra.utils.get_class(cfg._target_)
+
+    mp.spawn(cls,
+             args=(world_size,),
+             nprocs=world_size,
+             join=True)
+
+if __name__ == "__main__":
+    main()
